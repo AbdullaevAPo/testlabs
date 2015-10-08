7cb7a4675c0e3d546db08ef392ce40ac8b0a244e
montrose23 <balashovasofya@gmail.com>
2015-10-09 02:24:15 +0300

    ModuleTests2
    new fuckality

--- src/main/groovy/ModuleTests.groovy
+++ src/main/groovy/ModuleTests.groovy
@@ -8,27 +8,42 @@ import org.junit.Test
 public class PrologTableTest extends GroovyTestCase   {
 
     public void TestRules() {
+		assertEquals([[a, [b]]], PrologTable.parseAndFillRuleTable(['a->b']))
         assertEquals([[a, [c, b]], [b, [d]], [b, [p]]], PrologTable.parseAndFillRuleTable(['a->c&b', 'b->d|p']))
-        assertEquals([], PrologTable.parseAndFillRuleTable([]))
+        assertEquals([], PrologTable.parseAndFillRuleTable())
 		assertEquals([[, [b]]], PrologTable.parseAndFillRuleTable(['->b']))
-		
-		PrologTable.parseAndFillRuleTable(['a->'])
-		Assert.Fail() // ?
     }	
 	
-	public void TestFindPath() {
+	public void TestRulesEx() {
+		PrologTable.parseAndFillRuleTable(['a->bb'])
+		Assert.Fail("Size of elem must be equals 1") 	// ?
+		
+		PrologTable.parseAndFillRuleTable(['a->'])
+		Assert.Fail() 	// ?
+	}
+	
+	public void TestFacts() {
+		assertEquals('a', PrologTable.parseAndFillFactTable(['a']))
+		assertEquals('', PrologTable.parseAndFillFactTable([]))
+	}
+	
+	public void TestFindPathFull() {
 		def prologTable = new PrologTable(['a->c&b', 'b->d|p'], ['c'])
 		assertEquals(false, PrologTable.prologTable.findPath('a', 0))
         
 		prologTable = new PrologTable(['a->c&b', 'b->d|p'], ['c'])
 		assertEquals(true, PrologTable.prologTable.findPath('c', 0))
-		
+	}
+	
+	public void TestFindPathNoRules() {
 		prologTable = new PrologTable([], ['c'])
 		assertEquals(true, PrologTable.prologTable.findPath('c', 0))
 		
 		prologTable = new PrologTable([], ['c'])
 		assertEquals(false, PrologTable.prologTable.findPath('a', 0))
-		
+	}
+	
+	public void TestFindPathNoFacts() {
 		prologTable = new PrologTable(['a->c&b'], [])
 		assertEquals(false, PrologTable.prologTable.findPath('c', 0))
 		
@@ -38,5 +53,4 @@ public class PrologTableTest extends GroovyTestCase   {
 		prologTable = new PrologTable([], [])
 		assertEquals(false, PrologTable.prologTable.findPath('', 0))
 	}
-	
 }
